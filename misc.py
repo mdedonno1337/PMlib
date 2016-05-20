99ff2a072e96694acf5859c5b443726d9bf974b5
Marco De Donno <Marco.DeDonno@unil.ch>
2016-05-20 10:29:42 +0200

    Misc matrix manipulations

--- misc.py
+++ misc.py
@@ -9,3 +9,37 @@ import numpy as np
 def medianXY( data ):
     x, y = zip( *data )
     return np.median( x ), np.median( y )
+
+################################################################################
+#    Matrix manipulation
+################################################################################
+
+def matrixApply( point, matrix ):
+    inputx, inputy = point
+    return np.dot( np.matrix( "%f %f 1" % ( inputx, inputy ) ), np.matrix( matrix ) ).ravel().tolist()[0][0:2]
+
+def matrixListApply( list, matrix ):
+    ret = []
+
+    for p in list:
+        ret.append( matrixApply( p, matrix ) )
+
+    return ret
+
+def matrixMatrixApply( a, b ):
+    ret = []
+    for p in a.tolist():
+        ret.append( matrixApply( p, b ) )
+    
+    return np.matrix( ret )
+
+def multiDot( *argv ):
+    ret = argv[0]
+    
+    for m in argv[1:]:
+        ret = np.dot( 
+            ret,
+            m
+        )
+    
+    return ret
