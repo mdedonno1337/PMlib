bfc4d6331fb19829664d34f2b09a9947a4450b59
Marco De Donno <Marco.DeDonno@unil.ch>
2016-06-27 16:57:19 +0200

    cooNIST2PIL function

--- formatConverter.py
+++ formatConverter.py
@@ -17,4 +17,16 @@ def cooPIL2NIST( data, height, res ):
                 np.matrix( "%.6f 0 0; 0 %.6f 0; 0 0 1" % ( 25.4 / res, 25.4 / res ) )
             )
         )
+
+def cooNIST2PIL( data, height, res ):
+    if type( data ) == list:
+        return map( lambda x: cooNIST2PIL( x, height, res ), data )
+    else:
+        return matrixApply( 
+            data,
+            np.dot( 
+                np.matrix( "%.6f 0 0; 0 %.6f 0; 0 0 1" % ( res / 25.4, res / 25.4 ) ),
+                np.matrix( "1 0 0; 0 -1 0; 0 %.6f 1" % height )
+            )
+        )
         
